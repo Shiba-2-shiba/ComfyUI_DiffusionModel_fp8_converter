842b11999a36fcb7a82b3c3e1ac89e58c6068b1e
Shiba*2 <135475967+Shiba-2-shiba@users.noreply.github.com>
2024-08-29 04:28:52 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from .model_fp8_converter import ModelFP8ConverterNode
+from .clip_fp8_convert import ClipFP8ConverterNode  # ここを追加
 
 # ノードクラスのマッピングを設定
 NODE_CLASS_MAPPINGS = {
-    "ModelFP8ConverterNode": ModelFP8ConverterNode
+    "ModelFP8ConverterNode": ModelFP8ConverterNode,
+    "ClipFP8ConverterNode": ClipFP8ConverterNode  # ここを追加
 }
